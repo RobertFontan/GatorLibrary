b4a929fef0cc0e28e2b89058c55cdd9361908ef6
SammyMasri2 <sammymasri@ufl.edu>
2024-02-26 15:19:44 -0500

    Added Graduation year, major, and linkedIn inputs

--- client/src/routes/SignUp.jsx
+++ client/src/routes/SignUp.jsx
@@ -2,6 +2,14 @@
 import React, { useState } from 'react';
 import supabase from '../config/supabaseClient';
 
+const generateYears = (startYear) => {
+    const currentYear = new Date().getFullYear();
+    const years = [];
+    for (let year = startYear; year <= currentYear + 5; year++) {
+      years.push(year);
+    }
+    return years;
+  };
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
@@ -10,6 +18,11 @@ const SignUp = () => {
     const [lastName, setLastName] = useState('');
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
+    const [graduationYear, setGraduationYear] = useState('');
+    const [major, setMajor] = useState('');
+    const [linkedInUrl, setLinkedInUrl] = useState('');
+
+    const years = generateYears(2020);
 
     const handleSignUp = async (e) => {
         e.preventDefault();
@@ -58,6 +71,33 @@ const SignUp = () => {
                         style={{marginLeft: '10px', borderRadius:'10px', border:'1px solid #cccccc', fontFamily: 'Sarabun'}}
                     />
                 </div>   
+                <div className='inputGroup'>
+                    <select value={graduationYear}
+                        onChange={(e) => setGraduationYear(e.target.value)}
+                        style={{marginRight:'10px', borderRadius:'10px', border: '1px solid #cccccc', fontFamily: 'Sarabun', width: '100%'}}>
+                        <option value="">Select Graduation Year</option>
+                            {years.map((year) => (
+                                <option key={year} value={year}>{year}</option>
+                            ))}
+                </select>
+
+                    <select 
+                        value={major} 
+                        onChange={(e) => setMajor(e.target.value)}
+                        style={{marginLeft: '10px',marginRight: '10px', borderRadius:'10px', border:'1px solid #cccccc', fontFamily: 'Sarabun', width: '100%'}}>
+                        <option value="">Select Major</option>
+                        <option value="Computer Science CSE">Computer Science CSE</option>
+                        <option value="Computer Science CLAS">Computer Science CLAS</option>
+                    </select>
+
+                    <input
+                        type="text"
+                        placeholder="LinkedIn Profile URL"
+                        value={linkedInUrl}
+                        onChange={(e) => setLinkedInUrl(e.target.value)}
+                        style={{borderRadius:'10px', border:'1px solid #cccccc', fontFamily: 'Sarabun', width: '100%'}}
+                    />
+                </div>
                 <input
                     className='inputFullWidth'
                     type="email"
