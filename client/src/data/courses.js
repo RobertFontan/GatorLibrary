43ab1224caf5197189d4e193e464b00e80567709
aryantalekar20 <atalekar720@gmail.com>
2024-03-23 22:30:33 -0400

    degree checklist work

--- client/src/data/courses.js
+++ client/src/data/courses.js
@@ -3,17 +3,18 @@ export default [
         title: 'CEN4721 - Human Computer Interaction',
         playlistID: 'PLLssT5z_DsK_nusHL_Mjt87THSTlgrsyJ'
 
-    },
-    {
-        title: 'STA3032 - Engineering Statistics',
-        playlistID: 'PL7ysrfH_Y7lN_aWSUl6fFIVGWcjB4bALp'
-    },
-    {
-        title: 'COP4600 - Operating Systems',
-        playlistID: 'PLBlnK6fEyqRiVhbXDGLXDk_OQAeuVcp2O'
-    },  
-    {
-        title: 'CAP4641 - Natural Language Processing',
-        playlistID: 'PLQY2H8rRoyvzDbLUZkbudP-MFQZwNmU4S'
     }
+    // ,
+    // {
+    //     title: 'STA3032 - Engineering Statistics',
+    //     playlistID: 'PL7ysrfH_Y7lN_aWSUl6fFIVGWcjB4bALp'
+    // },
+    // {
+    //     title: 'COP4600 - Operating Systems',
+    //     playlistID: 'PLBlnK6fEyqRiVhbXDGLXDk_OQAeuVcp2O'
+    // },  
+    // {
+    //     title: 'CAP4641 - Natural Language Processing',
+    //     playlistID: 'PLQY2H8rRoyvzDbLUZkbudP-MFQZwNmU4S'
+    // }
 ]
--- client/src/routes/Profile.jsx
+++ client/src/routes/Profile.jsx
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import './Profile.css'
 import {Col, Nav, Row, Tab} from 'react-bootstrap';
+import Course from '../components/Course'
+import courses from '../data/courses'
 
 import Note from '../components/Note';
 
 import supabase from '../config/supabaseClient'
+
 
 
 
@@ -39,12 +42,114 @@ function Profile() {
    
       <h1>Profile</h1>
       <div>
-      <h3>Selected Courses</h3>
+      <h3>Degree Checklist</h3>
       <ul>
-        <li>Lorem ipsum dolor sit amet</li>
-        <li>Lorem ipsum dolor sit amet</li>
-        <li>Lorem ipsum dolor sit amet</li>
-        <li>Lorem ipsum dolor sit amet</li>
+      <div class="container"> 
+              <div id="accordion">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="DCbutton" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" color ="white">
+          CEN4721 - Human Computer Interaction
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        Recommended Videos:
+        <Col lg={9} className='left-screen'>
+            {courses.map(course => <Course course={course} />)}
+          </Col>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h5 class="mb-0">
+        <button class="DCbutton" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          COT3100 - Discrete Structures
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+        Recommended Videos:
+        <Col lg={9} className='left-screen'>
+            {courses.map(course => <Course course={course} />)}
+          </Col>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="DCbutton" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          COP46000 -  Operating Systems
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+        Recommended Videos:
+        <Col lg={9} className='left-screen'>
+            {courses.map(course => <Course course={course} />)}
+          </Col>
+      </div>
+    </div>
+  </div>
+    <div class="card">
+    <div class="card-header" id="headingFour">
+      <h5 class="mb-0">
+        <button class="DCbutton" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          STA3032 - Engineering Statistics
+        </button>
+      </h5>
+    </div>
+    <div id="collapseFour" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+      Recommended Videos:
+        <Col lg={9} className='left-screen'>
+            {courses.map(course => <Course course={course} />)}
+          </Col>
+      </div>
+    </div>
+  </div>
+    <div class="card">
+    <div class="card-header" id="headingFive">
+      <h5 class="mb-0">
+        <button class="DCbutton" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          MAC2311 - Calculus 1
+        </button>
+      </h5>
+    </div>
+    <div id="collapseFive" class="collapse" aria-labelledby="headingfive" data-parent="#accordion">
+      <div class="card-body">
+      Recommended Videos:
+        <Col lg={9} className='left-screen'>
+            {courses.map(course => <Course course={course} />)}
+          </Col></div>
+    </div>
+  </div>
+    <div class="card">
+    <div class="card-header" id="headingSix">
+      <h5 class="mb-0">
+        <button class="DCbutton" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+          MAC2312 - Calculus 2
+        </button>
+      </h5>
+    </div>
+    <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+      <div class="card-body">
+      Recommended Videos:
+        <Col lg={9} className='left-screen'>
+            {courses.map(course => <Course course={course} />)}
+          </Col></div>
+    </div>
+  </div>
+</div>
+
+      </div>   
       </ul>
     </div>
 
